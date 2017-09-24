2f1639a25992eccf897385dd4d46d70231981a6e
Wesley Song <wes.song@gmail.com>
2017-09-23 23:22:22 -0600

    Style for responsive menus

--- client/app/layout/components/header/components/header.component.js
+++ client/app/layout/components/header/components/header.component.js
@@ -2,33 +2,37 @@ const headerComponent = {
 	bindings: {},
 	controller: /*@ngInject*/ function () {
 		let ctrl = this;
-
+		ctrl.nav = nav;
 		ctrl.$onInit = function () {};
+
+		function nav() {
+			if(ctrl.dropdown) {
+				ctrl.dropdown = false;
+			}
+		}
 
 	},
 	template: `<div class="header-container">
     <div class="logo-container">
         <img src="http://via.placeholder.com/150x50">
     </div>
-    <div class="link-bar">
-        <div class="link-button">
-            <a ui-sref="app.home">Home</a>
-        </div>
-        <div class="link-button">
-            <a ui-sref="app.about">About</a>
-        </div>
-        <div class="link-button">
-            <a ui-sref="app.testimonials">Testimonials</a>
-        </div>
-        <div class="link-button">
-            <a ui-sref="app.projects">Projects</a>
-        </div>
-        <div class="link-button">
-            <a ui-sref="app.calendar">Calendar</a>
-        </div>
+    <div class="link-bar" ng-class="{'dropdown': $ctrl.dropdown}">
+        <a class="link-button" ui-sref="app.home" ui-sref-active="active" ng-click="$ctrl.nav()">Home</a>
+        <a class="link-button" ui-sref="app.about" ui-sref-active="active" ng-click="$ctrl.nav()">About</a>
+        <a class="link-button" ui-sref="app.testimonials" ui-sref-active="active"
+           ng-click="$ctrl.nav()">Testimonials</a>
+        <a class="link-button" ui-sref="app.projects" ui-sref-active="active" ng-click="$ctrl.nav()">Projects</a>
+        <a class="link-button" ui-sref="app.calendar" ui-sref-active="active" ng-click="$ctrl.nav()">Calendar</a>
     </div>
-    <div class="menu-container">
-        <i class="fa fa-user" aria-hidden="true"></i>
+    <div class="menu-container" ng-click="$ctrl.userMenu = !$ctrl.userMenu">
+        <i class="fa fa-user fa-2x" aria-hidden="true"></i>
+    </div>
+    <div class="user-menu" ng-if="$ctrl.userMenu">
+        <div class="menu-button">Login</div>
+        <div class="menu-button">Logout</div>
+    </div>
+    <div class="responsive-menu" ng-click="$ctrl.dropdown = !$ctrl.dropdown">
+        <i class="fa fa-bars fa-2x" aria-hidden="true"></i>
     </div>
 </div>`
 };
