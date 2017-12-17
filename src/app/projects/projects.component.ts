38340ff7e15c0e23969680d01347024da5d984f4
Wes Song <wes.song@gmail.com>
2017-12-17 16:47:44 -0700

    fixed projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  projects: Array = [];
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
