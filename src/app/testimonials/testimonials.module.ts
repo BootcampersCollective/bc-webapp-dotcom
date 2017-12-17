a25822da35f3c9e47255c3bca4159647b321a7f0
Wes Song <wes.song@gmail.com>
2017-12-17 16:32:55 -0700

    Get testimonials working with local service

--- src/app/testimonials/testimonials.module.ts
+++ src/app/testimonials/testimonials.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { TestimonialsRoutingModule } from './testimonials-routing.module';
 import { TestimonialsComponent } from './testimonials.component';
+import { TestimonialsService } from './testimonials.service';
 
 @NgModule({
   imports: [
     CommonModule,
     TestimonialsRoutingModule
   ],
+  providers: [ TestimonialsService ],
   declarations: [TestimonialsComponent]
 })
 export class TestimonialsModule { }
